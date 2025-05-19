433ae7bc9815477823d19992cb4c953cc541488b
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2025-05-19 07:49:36 +0300

    feat: Aggregations added

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,36 @@
+SELECT
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    COUNT(b.booking_id) AS total_bookings_made
+FROM
+    "User" u
+LEFT JOIN
+    Booking b ON u.user_id = b.user_id
+GROUP BY
+    u.user_id, u.first_name, u.last_name, u.email
+ORDER BY
+    total_bookings_made DESC, u.last_name ASC;
+
+WITH PropertyBookingCounts AS (
+    SELECT
+        p.property_id,
+        p.name AS property_name,
+        COUNT(b.booking_id) AS total_bookings_received
+    FROM
+        Property p
+    LEFT JOIN
+        Booking b ON p.property_id = b.property_id
+    GROUP BY
+        p.property_id, p.name
+)
+SELECT
+    pbc.property_id,
+    pbc.property_name,
+    pbc.total_bookings_received,
+    RANK() OVER (ORDER BY pbc.total_bookings_received DESC) AS property_rank
+FROM
+    PropertyBookingCounts pbc
+ORDER BY
+    property_rank ASC, pbc.property_name ASC;
